86d1a74f1c65b4a2e0c4e6c49df8efc7df9dc659
liceum20 <liceum20@S7-LICEUM-21.pcoip.ki.agh.edu.pl>
2017-02-12 12:10:11 +0100

    Working version 1.0

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,27 +1,51 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
+	
+	private List<BigDecimal> net = new ArrayList<BigDecimal>();
+	private List<BigDecimal> tax = new ArrayList<BigDecimal>();
+
 	public void addProduct(Product product) {
-		// TODO: implement
+		this.net.add(product.getPrice());
+		this.tax.add(product.getPrice().multiply(product.getTaxPercent()));
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		if (quantity < 1){
+			throw new IllegalArgumentException("Quantity must be a positive integer.");
+		}
+		this.net.add(product.getPrice()
+				.multiply(new BigDecimal(quantity)));
+		this.tax.add(product.getPrice()
+				.multiply(product.getTaxPercent())
+				.multiply(new BigDecimal(quantity)));
 	}
 
 	public BigDecimal getSubtotal() {
-		return null;
+		BigDecimal subTotal = new BigDecimal(0);
+		
+		for (BigDecimal i : net){
+			subTotal = subTotal.add(i);
+		}
+		return subTotal;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		BigDecimal subTax = new BigDecimal(0);
+		
+		for (BigDecimal i : tax){
+			subTax = subTax.add(i);
+		}
+		return subTax;
 	}
 
 	public BigDecimal getTotal() {
-		return null;
+		return this.getSubtotal().add(this.getTax());
 	}
 }
--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,32 @@ public abstract class Product {
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		
+		if (price == null || price.compareTo(new BigDecimal(0)) == -1) {
+			throw new IllegalArgumentException("Price cannot be null or negative.");
+		}
+		if (name == "" || name == null) {
+			throw new IllegalArgumentException("Name cannot be empty.");
+		}
+		
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
 	}
 
 	public String getName() {
-		return null;
+		return this.name;
 	}
 
 	public BigDecimal getPrice() {
-		return null;
+		return this.price;
 	}
 
 	public BigDecimal getTaxPercent() {
-		return null;
+		return this.taxPercent;
 	}
 
 	public BigDecimal getPriceWithTax() {
-		return null;
+		return this.price.multiply(this.taxPercent.add(new BigDecimal(1)));
 	}
 }
